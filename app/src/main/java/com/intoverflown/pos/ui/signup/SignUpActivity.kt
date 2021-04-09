e55eda3add2243f49efeb5893e5910e089f40cc7
RocqJones <jonesmbindyo@gmail.com>
2021-04-09 11:02:17 +0300

    SignUp Activity

---
+++ app/src/main/java/com/intoverflown/pos/ui/signup/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.intoverflown.pos.ui.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.intoverflown.pos.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
