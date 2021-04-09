eb369ec70cd4953ad1b1ccb8fdb7769cafc69132
RocqJones <jonesmbindyo@gmail.com>
2021-04-09 11:00:28 +0300

    Login activity

---
+++ app/src/main/java/com/intoverflown/pos/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.intoverflown.pos.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.intoverflown.pos.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
