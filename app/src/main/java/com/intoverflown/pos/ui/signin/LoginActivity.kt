bc98a10ebe417f467f05767c23a7393b459fd122
RocqJones <jonesmbindyo@gmail.com>
2021-04-10 13:27:47 +0300

    login navigate to signup and data binding

--- app/src/main/java/com/intoverflown/pos/ui/signin/LoginActivity.kt
+++ app/src/main/java/com/intoverflown/pos/ui/signin/LoginActivity.kt
@@ -1,12 +1,25 @@
 package com.intoverflown.pos.ui.signin
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.intoverflown.pos.R
+import androidx.appcompat.app.AppCompatActivity
+import com.intoverflown.pos.databinding.ActivityLoginBinding
+import com.intoverflown.pos.ui.signup.SignUpActivity
 
 class LoginActivity : AppCompatActivity() {
+
+    private var binding: ActivityLoginBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+
+        binding!!.loginNavToSignUp.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent)
+        }
     }
 }
