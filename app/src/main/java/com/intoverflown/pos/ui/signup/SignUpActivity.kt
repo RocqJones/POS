336d1c300dd762ee9406cce02cbcb8b09c5ce75a
RocqJones <jonesmbindyo@gmail.com>
2021-04-10 13:17:37 +0300

    signUp back btn

--- app/src/main/java/com/intoverflown/pos/ui/signup/SignUpActivity.kt
+++ app/src/main/java/com/intoverflown/pos/ui/signup/SignUpActivity.kt
@@ -1,12 +1,25 @@
 package com.intoverflown.pos.ui.signup
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.intoverflown.pos.R
+import androidx.appcompat.app.AppCompatActivity
+import com.intoverflown.pos.databinding.ActivitySignUpBinding
+import com.intoverflown.pos.ui.signin.LoginActivity
 
 class SignUpActivity : AppCompatActivity() {
+
+    private var binding : ActivitySignUpBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+
+        binding!!.signUpBackBtn.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent)
+        }
     }
 }
