f1331a02ba829ef88534bf0da4a7212e7ef33f37
RocqJones <jonesmbindyo@gmail.com>
2021-04-12 11:41:51 +0300

    login: intent to reset password

--- app/src/main/java/com/intoverflown/pos/ui/signin/LoginActivity.kt
+++ app/src/main/java/com/intoverflown/pos/ui/signin/LoginActivity.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.intoverflown.pos.databinding.ActivityLoginBinding
-import com.intoverflown.pos.ui.signup.SignUpActivity
+import com.intoverflown.pos.ui.resetpassword.ResetPwdActivity
 
 class LoginActivity : AppCompatActivity() {
 
@@ -16,8 +16,13 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding!!.root)
 
-        binding!!.loginNavToSignUp.setOnClickListener {
-            val intent = Intent(this, SignUpActivity::class.java)
+//        binding!!.loginNavToSignUp.setOnClickListener {
+//            val intent = Intent(this, SignUpActivity::class.java)
+//            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+//            startActivity(intent)
+//        }
+        binding!!.loginForgotPwd.setOnClickListener {
+            val intent = Intent(this, ResetPwdActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
             startActivity(intent)
         }
