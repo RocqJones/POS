f004521592335d72d6bd2c52a847906a31cf45da
RocqJones <jonesmbindyo@gmail.com>
2021-04-17 16:00:57 +0300

    removed appBar configs

--- app/src/main/java/com/intoverflown/pos/MainActivity.kt
+++ app/src/main/java/com/intoverflown/pos/MainActivity.kt
@@ -7,24 +7,28 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.intoverflown.pos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+        //setContentView(R.layout.activity_main)
 
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        val navView: BottomNavigationView = binding!!.navView //findViewById(R.id.nav_view)
 
-        // val navController = findNavController(R.id.nav_host_fragment)
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
 
-        // Passing each menu ID as a set of Ids because each menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_settings, R.id.navigation_profile))
-
-        setupActionBarWithNavController(navController, appBarConfiguration)
+//        // Passing each menu ID as a set of Ids because each menu should be considered as top level destinations.
+//        val appBarConfiguration = AppBarConfiguration(setOf(
+//                R.id.navigation_home, R.id.navigation_settings, R.id.navigation_profile))
+//
+//        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 }
