96807fe70138e0ff446eac1eb34fab7e4dc468cf
RocqJones <jonesmbindyo@gmail.com>
2021-04-20 10:33:03 +0300

    intent to notification activity

--- app/src/main/java/com/intoverflown/pos/MainActivity.kt
+++ app/src/main/java/com/intoverflown/pos/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.intoverflown.pos
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
@@ -8,6 +9,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.intoverflown.pos.databinding.ActivityMainBinding
+import com.intoverflown.pos.ui.notification.NotificationActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,5 +32,11 @@ class MainActivity : AppCompatActivity() {
 //
 //        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        binding!!.mainNotification.setOnClickListener {
+            val intent = Intent(this, NotificationActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent)
+        }
     }
 }
