9490521c9ad1d75b81cfa1cfa45a578961d56df8
RocqJones <jonesmbindyo@gmail.com>
2021-04-22 14:34:55 +0300

    up btn intent to parent activity.

--- app/src/main/java/com/intoverflown/pos/ui/registerbranch/NewBranchActivity.kt
+++ app/src/main/java/com/intoverflown/pos/ui/registerbranch/NewBranchActivity.kt
@@ -1,7 +1,9 @@
 package com.intoverflown.pos.ui.registerbranch
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.intoverflown.pos.MainActivity
 import com.intoverflown.pos.databinding.ActivityNewBranchBinding
 
 class NewBranchActivity : AppCompatActivity() {
@@ -12,5 +14,11 @@ class NewBranchActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityNewBranchBinding.inflate(layoutInflater)
         setContentView(binding!!.root)
+
+        binding!!.upBtn.setOnClickListener {
+            val x = Intent(this, MainActivity::class.java)
+            x.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(x)
+        }
     }
 }
