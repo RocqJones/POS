04e48a611468a44efc2b4f0094af516b055b4675
RocqJones <jonesmbindyo@gmail.com>
2021-04-23 20:36:49 +0300

    api response handler

---
+++ app/src/main/java/com/intoverflown/pos/network/ApiResponseHandler.kt
@@ -0,0 +1,15 @@
+package com.intoverflown.pos.network
+
+import okhttp3.ResponseBody
+
+/** Generic class that handle both success and failure either by network or api
+ * We will use this sealed class to wrap or handle success and errors properly */
+sealed class ApiResponseHandler<out T> {
+    data class Success<out T>(val value: T) : ApiResponseHandler<T>()
+
+    data class Failure(
+        val isNetworkError : Boolean,
+        val errorCode : Int?,
+        val errorBody: ResponseBody?
+    )
+}
