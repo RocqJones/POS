6e7de44d20080267819cb4c8c51dc5e83b81f0fb
RocqJones <jonesmbindyo@gmail.com>
2021-04-24 10:46:39 +0300

    api interface

--- app/src/main/java/com/intoverflown/pos/network/ApiInterface.kt
+++ app/src/main/java/com/intoverflown/pos/network/ApiInterface.kt
@@ -1,6 +1,6 @@
 package com.intoverflown.pos.network
 
-import com.intoverflown.pos.api_responses.loginResponse
+import com.intoverflown.pos.api_responses.LoginResponse
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
@@ -12,5 +12,5 @@ interface ApiInterface {
     suspend fun login(
         @Field("username") username: String,
         @Field("password") password: String
-    ) : loginResponse
+    ) : LoginResponse
 }
