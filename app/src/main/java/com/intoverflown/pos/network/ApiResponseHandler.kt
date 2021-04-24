ca9742d9bb0ad3d8dc6b536792c55b9df2f8291c
RocqJones <jonesmbindyo@gmail.com>
2021-04-24 11:09:00 +0300

    extend Failure class with "ApiResponseHandler" to return 'Nothing'

--- app/src/main/java/com/intoverflown/pos/network/ApiResponseHandler.kt
+++ app/src/main/java/com/intoverflown/pos/network/ApiResponseHandler.kt
@@ -3,7 +3,8 @@ package com.intoverflown.pos.network
 import okhttp3.ResponseBody
 
 /** Generic class that handle both success and failure either by network or api
- * We will use this sealed class to wrap or handle success and errors properly */
+ * We will use this sealed class to wrap or handle success and errors properly
+ * Make sure to extend Failure class with "ApiResponseHandler" to return 'Nothing' */
 sealed class ApiResponseHandler<out T> {
     data class Success<out T>(val value: T) : ApiResponseHandler<T>()
 
@@ -11,5 +12,5 @@ sealed class ApiResponseHandler<out T> {
         val isNetworkError : Boolean,
         val errorCode : Int?,
         val errorBody: ResponseBody?
-    )
+    ) : ApiResponseHandler<Nothing>()
 }
