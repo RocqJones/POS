c27f4448d63a19f4d36620d4e6a13aa57efb1af9
RocqJones <jonesmbindyo@gmail.com>
2021-04-26 13:06:38 +0300

    api client logging on DEBUG

--- app/src/main/java/com/intoverflown/pos/network/ApiClient.kt
+++ app/src/main/java/com/intoverflown/pos/network/ApiClient.kt
@@ -1,6 +1,9 @@
 package com.intoverflown.pos.network
 
+import com.intoverflown.pos.BuildConfig
 import com.intoverflown.pos.utils.Constants.Companion.BASE_URL
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -11,6 +14,13 @@ class ApiClient {
     ) : Api {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(OkHttpClient.Builder().also { client ->
+                if (BuildConfig.DEBUG) {
+                    val logging = HttpLoggingInterceptor()
+                    logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+                    client.addInterceptor(logging)
+                }
+            }.build())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(api)
