186f4f6fbcc787dc30d69c27cd2dd331a29e6673
RocqJones <jonesmbindyo@gmail.com>
2021-04-28 14:52:33 +0300

    base url

--- app/src/main/java/com/intoverflown/pos/utils/Constants.kt
+++ app/src/main/java/com/intoverflown/pos/utils/Constants.kt
@@ -3,6 +3,6 @@ package com.intoverflown.pos.utils
 class Constants {
 
     companion object {
-        const val BASE_URL = "https://pos.bantucore.com/"
+        const val BASE_URL = "https://pos.bantucore.com/Service/POS/API/v1/"
     }
 }
