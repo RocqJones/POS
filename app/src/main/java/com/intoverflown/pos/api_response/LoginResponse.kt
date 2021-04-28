2d3b3f121cea10dd3cb1397398c5c592b8c23669
RocqJones <jonesmbindyo@gmail.com>
2021-04-28 14:19:20 +0300

    api response params structure

--- app/src/main/java/com/intoverflown/pos/api_response/LoginResponse.kt
+++ app/src/main/java/com/intoverflown/pos/api_response/LoginResponse.kt
@@ -1,10 +1,10 @@
 package com.intoverflown.pos.api_response
 
 data class LoginResponse(
-    val FirstName: String,
     val Id: String,
+    val FirstName: String,
     val LastName: String,
+    val Username: String,
     val Role: String,
-    val Token: String,
-    val Username: String
+    val Token: String
 )
