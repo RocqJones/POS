b1cf62bc4ce3cd9f6b819d1cf98d4db87eb883d1
RocqJones <jonesmbindyo@gmail.com>
2021-04-29 21:57:46 +0300

    add product POST

--- app/src/main/java/com/intoverflown/pos/ui/inventory/addproduct/AddProductActivity.kt
+++ app/src/main/java/com/intoverflown/pos/ui/inventory/addproduct/AddProductActivity.kt
@@ -1,10 +1,13 @@
 package com.intoverflown.pos.ui.inventory.addproduct
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.android.volley.toolbox.Volley
+import com.intoverflown.pos.data.SharedPreferenceManager
 import com.intoverflown.pos.databinding.ActivityAddProductBinding
-import com.intoverflown.pos.ui.inventory.InventoryActivityMain
+import com.intoverflown.pos.utils.Constants
+import org.json.JSONObject
 
 class AddProductActivity : AppCompatActivity() {
 
@@ -14,10 +17,24 @@ class AddProductActivity : AppCompatActivity() {
         binding = ActivityAddProductBinding.inflate(layoutInflater)
         setContentView(binding!!.root)
 
+//        val uid = SharedPreferenceManager.getInstance(applicationContext).getUser()
+//        val token = SharedPreferenceManager.getInstance(applicationContext).getUser().Token
+//        Log.d("uid", uid.toString())
+//        Log.d("token pref", token.toString())
+
         binding!!.addBackBtn.setOnClickListener {
-            val i = Intent(this, InventoryActivityMain::class.java)
-            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-            startActivity(i)
+//            val i = Intent(this, InventoryActivityMain::class.java)
+//            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+//            startActivity(i)
+            val url = Constants.BASE_URL + "MerchantBranch/Create"
+//            addNewProduct(url, uid.toString(), token.toString())
         }
+    }
+
+    private fun addNewProduct(url: String, uid: String, token: String) {
+        val queue = Volley.newRequestQueue(applicationContext)
+        val jsonObject = JSONObject()
+
+
     }
 }
