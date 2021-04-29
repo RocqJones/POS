d440561e700c6e908dc6154bd5e4768a0ca2b98c
RocqJones <jonesmbindyo@gmail.com>
2021-04-29 21:59:22 +0300

    response constructor nullable

--- app/src/main/java/com/intoverflown/pos/api_response/LoginResponse.kt
+++ app/src/main/java/com/intoverflown/pos/api_response/LoginResponse.kt
@@ -1,10 +1,10 @@
 package com.intoverflown.pos.api_response
 
 data class LoginResponse(
-    val Id: String,
-    val FirstName: String,
-    val LastName: String,
-    val Username: String,
-    val Role: String,
-    val Token: String
+    val Id: String?,
+    val FirstName: String?,
+    val LastName: String?,
+    val Username: String?,
+    val Role: String?,
+    val Token: String?
 )
