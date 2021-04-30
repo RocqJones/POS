f2a927078de4246b1e93f8b168dec424e4c57c4b
RocqJones <jonesmbindyo@gmail.com>
2021-04-30 20:28:23 +0300

    migrations from kt to java: create merchant branch

---
+++ app/src/main/java/com/intoverflown/pos/ui/registerbranch/NewBranchActivity.java
@@ -0,0 +1,37 @@
+package com.intoverflown.pos.ui.registerbranch;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.intoverflown.pos.databinding.ActivityNewBranchBinding;
+
+import java.sql.DataTruncation;
+
+public class NewBranchActivity extends AppCompatActivity {
+
+    private ActivityNewBranchBinding binding;
+
+    public SharedPreferences preferences;
+    public String SHARED_PREF_NAME = "pos_pref";
+    public String KEY_ID = "Id";
+    public String KEY_TOKEN = "Token";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityNewBranchBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        preferences = this.getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE);
+
+        String uid = preferences.getString(KEY_ID, "Id");
+        String token = preferences.getString(KEY_TOKEN, "Token");
+
+        Log.d("uid new branch", uid);
+        Log.d("token new branch", token);
+    }
+}
