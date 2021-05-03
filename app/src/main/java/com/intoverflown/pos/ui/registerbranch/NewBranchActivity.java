debe2001359ad979120372eaec322ce66b61408e
RocqJones <jonesmbindyo@gmail.com>
2021-05-03 14:36:38 +0300

    replaced JsonObjectRequest with JsonArrayRequest and parsing auth token param

--- app/src/main/java/com/intoverflown/pos/ui/registerbranch/NewBranchActivity.java
+++ app/src/main/java/com/intoverflown/pos/ui/registerbranch/NewBranchActivity.java
@@ -1,21 +1,41 @@
 package com.intoverflown.pos.ui.registerbranch;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.intoverflown.pos.MainActivity;
 import com.intoverflown.pos.databinding.ActivityNewBranchBinding;
+import com.intoverflown.pos.utils.Constants;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class NewBranchActivity extends AppCompatActivity {
 
     private ActivityNewBranchBinding binding;
 
     public SharedPreferences preferences;
+    public SharedPreferences.Editor editor;
     public String SHARED_PREF_NAME = "pos_pref";
     public String KEY_ID = "Id";
     public String KEY_TOKEN = "Token";
+
+    String uid;
+    String token;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +44,178 @@ public class NewBranchActivity extends AppCompatActivity {
         binding = ActivityNewBranchBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        binding.upBtn.setOnClickListener(v -> {
+            Intent i = new Intent(NewBranchActivity.this, MainActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(i);
+        });
+
         preferences = this.getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE);
 
-        String uid = preferences.getString(KEY_ID, "Id");
-        String token = preferences.getString(KEY_TOKEN, "Token");
+        uid = preferences.getString(KEY_ID, "Id");
+        token = preferences.getString(KEY_TOKEN, "Token");
 
         Log.d("uid new branch", uid);
         Log.d("token new branch", token);
+
+        binding.regMerchantBtn.setOnClickListener(v -> {
+            String url = Constants.BASE_URL + "MerchantBranch/Create";
+            postNewMerchant(url);
+        });
     }
+
+    private void postNewMerchant(String url) {
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        JSONArray array = new JSONArray();
+        JSONObject jsonObjects = new JSONObject();
+
+        String region = binding.regMerchantRegion.getText().toString().trim();
+        String address = binding.regMerchantAddress.getText().toString().trim();
+        String branchName = binding.regMerchantMerchantBranch.getText().toString().trim();
+        String contactPerson = binding.regMerchantMerchantName.getText().toString().trim();
+        String phone = binding.regMerchantPhone.getText().toString().trim();
+
+        if (region.isEmpty() && address.isEmpty() &&
+                branchName.isEmpty() && contactPerson.isEmpty() && phone.isEmpty()) {
+            Toast.makeText(this, "Enter all fields!!", Toast.LENGTH_SHORT).show();
+        } else {
+            ProgressDialog progressDialog = new ProgressDialog(NewBranchActivity.this);
+            progressDialog.setMessage("Creating new branch merchant!");
+            progressDialog.show();
+
+            try {
+                jsonObjects.put("RegionId", region);
+                jsonObjects.put("BranchName", branchName);
+                jsonObjects.put("Address", address);
+                jsonObjects.put("Phone", phone);
+                jsonObjects.put("ContactPerson", contactPerson);
+                jsonObjects.put("CreatedById", uid);
+
+                array = new JSONArray("["+jsonObjects.toString()+"]");
+                Log.d("Post", array.toString());
+            } catch (final JSONException e) {
+                e.printStackTrace();
+            }
+
+            JsonArrayRequest  jsonObjectRequest = new JsonArrayRequest(com.android.volley.Request.Method.POST,
+                    url, array, response -> {
+                try {
+                    Log.d("connection", response.toString());
+                    progressDialog.dismiss();
+
+                    Log.d("response", response.toString());
+
+                    for(int i=0; i < response.length(); i++) {
+                        JSONObject jsonObj = response.getJSONObject(i);
+                        Log.d("uid merchant branch", jsonObj.optString("uniqueId"));
+
+                        // store id
+                        editor = preferences.edit();
+                        editor.putString("merchantBranchId", jsonObj.optString("uniqueId"));
+                        editor.apply();
+                    }
+
+                    Toast.makeText(this, "Created Successfully!", Toast.LENGTH_SHORT).show();
+
+                    intentBackHome();
+                } catch (Exception e) {
+                    Toast.makeText(this, "Error occurred\nCheck logs!", Toast.LENGTH_LONG).show();
+                    Log.i("new branch", Log.getStackTraceString(e));
+                }
+            }, error -> {
+                progressDialog.dismiss();
+                Log.e("error", error.toString());
+                Toast.makeText(NewBranchActivity.this, "Failed to create branch!", Toast.LENGTH_SHORT).show();
+            }) {
+                @Override
+                public Map<String, String> getHeaders() {
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("Content-Type", "application/json");
+                    params.put("Authorization", "Bearer " + token);
+                    return params;
+                }
+
+                @Override
+                public String getBodyContentType() {
+                    return "application/json";
+                }
+            };
+
+            jsonObjectRequest.setRetryPolicy(new DefaultRetryPolicy(60000,
+                    DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+
+            queue.add(jsonObjectRequest);
+        }
+    }
+
+    private void intentBackHome() {
+        Intent j = new Intent(NewBranchActivity.this, MainActivity.class);
+        j.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(j);
+        finish();
+    }
+
+//    public void sample(String url) {
+//        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+//        JSONArray array = new JSONArray();
+//        JSONObject jsonObjects = new JSONObject();
+//        try {
+//
+////            "MerchantName": "Elen",
+////                    "MerchantTypeId": "TypeB",
+////                    "CountryId": "Kenya",
+////                    "CreatedById": "1aadb13d-5252-47d6-a157-10e8892dbb57"
+//
+//            jsonObjects.put("MerchantName", "EEE");
+//            jsonObjects.put("MerchantTypeId", "TypeA");
+//            jsonObjects.put("CountryId", "KE");
+//            jsonObjects.put("ContactPerson", "Jokkk");
+//            jsonObjects.put("CreatedById", uid);
+//
+//            array = new JSONArray("["+jsonObjects.toString()+"]");
+//
+//            Log.d("TAG" + "SAMPLE", array.toString());
+//        } catch (final JSONException e) {
+//            e.printStackTrace();
+//
+//        }
+//
+//        JsonArrayRequest jsonObjectRequest = new JsonArrayRequest(com.android.volley.Request.Method.POST,
+//                url, array, response -> {
+//
+//                    try {
+//
+//                        Log.d("TEXT", response.toString());
+//
+//                        Toast.makeText(getApplicationContext(), response.toString(), Toast.LENGTH_LONG).show();
+//
+//
+//                    } catch (Exception e) {
+//
+//                        Toast.makeText(NewBranchActivity.this, Log.getStackTraceString(e) + "",
+//                                Toast.LENGTH_LONG).show();
+//                        Log.i("TAG", Log.getStackTraceString(e));
+//                    }
+//                }, error -> Log.e("error", error.toString())) {
+//            @Override
+//            public Map<String, String> getHeaders() throws AuthFailureError {
+//                Map<String, String> params = new HashMap<String, String>();
+//                params.put("Content-Type", "application/json");
+//                params.put("Authorization", "Bearer " + token);
+//                return params;
+//            }
+//
+//            @Override
+//            public String getBodyContentType() {
+//                return "application/json";
+//            }
+//        };
+//        jsonObjectRequest.setRetryPolicy(new
+//
+//                DefaultRetryPolicy(
+//                60000,
+//                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+//                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+//        queue.add(jsonObjectRequest);
+//    }
 }
