85d7ec253ada1e7b6cfb57a76de760cd4a601c19
RocqJones <jonesmbindyo@gmail.com>
2021-05-03 15:26:17 +0300

    migration: AddProductActivity kt to Java

---
+++ app/src/main/java/com/intoverflown/pos/ui/inventory/addproduct/AddProductActivity.java
@@ -0,0 +1,36 @@
+package com.intoverflown.pos.ui.inventory.addproduct;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.intoverflown.pos.databinding.ActivityAddProductBinding;
+import com.intoverflown.pos.ui.inventory.InventoryActivityMain;
+import com.intoverflown.pos.utils.Constants;
+
+public class AddProductActivity extends AppCompatActivity {
+
+    private ActivityAddProductBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityAddProductBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.addBackBtn.setOnClickListener(v -> {
+            Intent i = new  Intent(AddProductActivity.this, InventoryActivityMain.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(i);
+        });
+
+        binding.addSaveBtn.setOnClickListener(v -> {
+            String url = Constants.BASE_URL + "Product/Create";
+            postProduct(url);
+        });
+    }
+
+    private void postProduct(String url) { }
+}
