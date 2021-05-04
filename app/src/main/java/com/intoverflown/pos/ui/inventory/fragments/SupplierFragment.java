1b15d31cdc23692dab577cbcb9da84f38a10e8a1
RocqJones <jonesmbindyo@gmail.com>
2021-05-04 15:42:46 +0300

    supplier fragment

---
+++ app/src/main/java/com/intoverflown/pos/ui/inventory/fragments/SupplierFragment.java
@@ -0,0 +1,24 @@
+package com.intoverflown.pos.ui.inventory.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.intoverflown.pos.databinding.SupplierFragmentBinding;
+
+public class SupplierFragment extends Fragment {
+
+    SupplierFragmentBinding binding;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        binding = SupplierFragmentBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+}
