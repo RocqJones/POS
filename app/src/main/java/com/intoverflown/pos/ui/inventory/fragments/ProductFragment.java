e9d0b7a8d0ae8736d0ac77ed812b59f25cd7d645
RocqJones <jonesmbindyo@gmail.com>
2021-05-04 16:15:39 +0300

    Product intents

--- app/src/main/java/com/intoverflown/pos/ui/inventory/fragments/ProductFragment.java
+++ app/src/main/java/com/intoverflown/pos/ui/inventory/fragments/ProductFragment.java
@@ -1,5 +1,6 @@
 package com.intoverflown.pos.ui.inventory.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +10,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.intoverflown.pos.databinding.ProductFragmentBinding;
+import com.intoverflown.pos.ui.inventory.addproduct.AddProductActivity;
 
 public class ProductFragment extends Fragment {
 
@@ -19,6 +21,12 @@ public class ProductFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
 
         binding = ProductFragmentBinding.inflate(inflater, container, false);
+
+        binding.fab.setOnClickListener(v -> {
+            Intent i = new Intent(ProductFragment.this.getContext(), AddProductActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(i);
+        });
 
         return binding.getRoot();
     }
