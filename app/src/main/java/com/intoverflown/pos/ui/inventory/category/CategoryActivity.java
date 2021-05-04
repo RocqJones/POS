f7d3a1b47a934ad78354fb6a75fd65f4852d66f1
RocqJones <jonesmbindyo@gmail.com>
2021-05-04 11:16:51 +0300

    add CategoryActivity

---
+++ app/src/main/java/com/intoverflown/pos/ui/inventory/category/CategoryActivity.java
@@ -0,0 +1,148 @@
+package com.intoverflown.pos.ui.inventory.category;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.intoverflown.pos.databinding.ActivityCategoryBinding;
+import com.intoverflown.pos.ui.inventory.InventoryActivityMain;
+import com.intoverflown.pos.ui.inventory.addproduct.AddProductActivity;
+import com.intoverflown.pos.utils.Constants;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CategoryActivity extends AppCompatActivity {
+
+    private ActivityCategoryBinding binding;
+
+    String uid;
+    String token;
+    public SharedPreferences preferences;
+    public SharedPreferences.Editor editor;
+    public String SHARED_PREF_NAME = "pos_pref";
+    public String KEY_ID = "Id";
+    public String KEY_TOKEN = "Token";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityCategoryBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.categoryBackBtn.setOnClickListener(v -> {
+            Intent i = new Intent(CategoryActivity.this, InventoryActivityMain.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(i);
+        });
+
+        /* Get uid and token */
+        preferences = this.getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE);
+
+        uid = preferences.getString(KEY_ID, "Id");
+        token = preferences.getString(KEY_TOKEN, "Token");
+
+        Log.d("uid category", uid);
+        Log.d("token category", token);
+
+        binding.categorySaveBtn.setOnClickListener(v -> {
+            String url = Constants.BASE_URL + "ProductCategory/Create";
+            createCategory(url);
+        });
+    }
+
+    private void createCategory(String url) {
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        JSONArray array = new JSONArray();
+        JSONObject jsonObjects = new JSONObject();
+
+        String name = binding.categoryName.getText().toString().trim();
+        String remarks = binding.categoryRemarks.getText().toString().trim();
+
+        if (name.isEmpty() && remarks.isEmpty()) {
+            Toast.makeText(this, "Enter all fields!!", Toast.LENGTH_SHORT).show();
+        } else {
+            ProgressDialog progressDialog = new ProgressDialog(CategoryActivity.this);
+            progressDialog.setMessage("Creating new category...");
+            progressDialog.show();
+
+            try {
+                jsonObjects.put("Name", name);
+                jsonObjects.put("Remarks", remarks);
+                jsonObjects.put("createdById", uid);
+
+                array = new JSONArray("["+jsonObjects.toString()+"]");
+                Log.d("Post", array.toString());
+
+            } catch (final JSONException e) {
+                e.printStackTrace();
+            }
+
+            JsonArrayRequest jsonObjectRequest = new JsonArrayRequest(com.android.volley.Request.Method.POST,
+                    url, array, response -> {
+                try {
+                    Log.d("response", response.toString());
+
+                    for(int i=0; i < response.length(); i++) {
+                        JSONObject jsonObj = response.getJSONObject(i);
+                        Log.d("uid merchant branch", jsonObj.optString("uniqueId"));
+
+                        // store id
+                        editor = preferences.edit();
+                        editor.putString("categoryId", jsonObj.optString("uniqueId"));
+                        editor.apply();
+                    }
+
+                    progressDialog.dismiss();
+                    Toast.makeText(this, "Created Successfully!", Toast.LENGTH_SHORT).show();
+                    proceedToCreateProduct();
+                } catch (Exception e) {
+                    Toast.makeText(this, "Error occurred\nCheck logs!", Toast.LENGTH_LONG).show();
+                    Log.i("new branch", Log.getStackTraceString(e));
+                }
+            }, error -> {
+                progressDialog.dismiss();
+                Log.e("error", error.toString());
+                Toast.makeText(CategoryActivity.this, "Failed to create branch!", Toast.LENGTH_SHORT).show();
+            }) {
+                @Override
+                public Map<String, String> getHeaders() {
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("Content-Type", "application/json");
+                    params.put("Authorization", "Bearer " + token);
+                    return params;
+                }
+
+                @Override
+                public String getBodyContentType() {
+                    return "application/json";
+                }
+            };
+
+            jsonObjectRequest.setRetryPolicy(new DefaultRetryPolicy(60000,
+                    DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+
+            queue.add(jsonObjectRequest);
+        }
+    }
+
+    private void proceedToCreateProduct() {
+        Intent j = new Intent(CategoryActivity.this, AddProductActivity.class);
+        j.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(j);
+        finish();
+    }
+}
