f6fc695759a8b20e56e1b6d1d6e607cff0549434
RocqJones <jonesmbindyo@gmail.com>
2021-05-05 07:42:25 +0300

    SupplierFragment add float btn

--- app/src/main/java/com/intoverflown/pos/ui/inventory/fragments/SupplierFragment.java
+++ app/src/main/java/com/intoverflown/pos/ui/inventory/fragments/SupplierFragment.java
@@ -1,5 +1,6 @@
 package com.intoverflown.pos.ui.inventory.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +10,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.intoverflown.pos.databinding.SupplierFragmentBinding;
+import com.intoverflown.pos.ui.inventory.addsupplier.AddSupplierActivity;
 
 public class SupplierFragment extends Fragment {
 
@@ -17,8 +19,13 @@ public class SupplierFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
         binding = SupplierFragmentBinding.inflate(inflater, container, false);
+        binding.fab.setOnClickListener(v -> {
+            Intent i = new Intent(SupplierFragment.this.getContext(), AddSupplierActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(i);
+        });
+
         return binding.getRoot();
     }
 }
