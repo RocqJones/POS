4a271af830349cb43a9c21b44c31ae77b5712e0b
RocqJones <jonesmbindyo@gmail.com>
2021-05-10 15:01:50 +0300

    orders activity

--- app/src/main/java/com/intoverflown/pos/ui/orders/OrdersActivity.java
+++ app/src/main/java/com/intoverflown/pos/ui/orders/OrdersActivity.java
@@ -1,18 +1,15 @@
 package com.intoverflown.pos.ui.orders;
 
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
-
-import androidx.viewpager.widget.ViewPager;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-
+import com.intoverflown.pos.R;
 import com.intoverflown.pos.ui.orders.ui.main.SectionsPagerAdapter;
 
 public class OrdersActivity extends AppCompatActivity {
