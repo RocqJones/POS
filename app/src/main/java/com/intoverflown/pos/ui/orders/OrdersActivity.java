7e67cb01ee8b12962881baa84d4be6e186bb75b0
RocqJones <jonesmbindyo@gmail.com>
2021-05-14 22:48:51 +0300

    introduced ViewBinding and added up btn

--- app/src/main/java/com/intoverflown/pos/ui/orders/OrdersActivity.java
+++ app/src/main/java/com/intoverflown/pos/ui/orders/OrdersActivity.java
@@ -1,31 +1,38 @@
 package com.intoverflown.pos.ui.orders;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
-import com.intoverflown.pos.R;
+import com.intoverflown.pos.MainActivity;
+import com.intoverflown.pos.databinding.ActivityOrdersBinding;
 import com.intoverflown.pos.ui.orders.ui.main.SectionsPagerAdapter;
 
 public class OrdersActivity extends AppCompatActivity {
 
+    private ActivityOrdersBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_orders);
-        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = findViewById(R.id.view_pager);
-        viewPager.setAdapter(sectionsPagerAdapter);
-        TabLayout tabs = findViewById(R.id.tabs);
-        tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = findViewById(R.id.fab);
+        binding = ActivityOrdersBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show());
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = binding.viewPager;
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = binding.tabs;
+        tabs.setupWithViewPager(viewPager);
+
+        binding.title.setOnClickListener(v -> {
+            Intent i = new Intent(this, MainActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(i);
+            finish();
+        });
     }
 }
