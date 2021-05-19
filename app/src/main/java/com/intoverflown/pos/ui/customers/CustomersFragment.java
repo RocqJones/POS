836e1532c1da2fe3dadf092286694c9f31c695a3
RocqJones <jonesmbindyo@gmail.com>
2021-05-19 11:41:11 +0300

    Customer getRequest response Success

--- app/src/main/java/com/intoverflown/pos/ui/customers/CustomersFragment.java
+++ app/src/main/java/com/intoverflown/pos/ui/customers/CustomersFragment.java
@@ -1,7 +1,9 @@
 package com.intoverflown.pos.ui.customers;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,12 +11,33 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.intoverflown.pos.databinding.FragmentCustomersBinding;
+import com.intoverflown.pos.patterns.MySingleton;
 import com.intoverflown.pos.ui.customers.addcustomers.AddCustomerActivity;
+import com.intoverflown.pos.utils.Constants;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static android.content.Context.MODE_PRIVATE;
 
 public class CustomersFragment extends Fragment {
 
     private FragmentCustomersBinding binding;
+
+    String token;
+    Integer merchantId;
+
+    public SharedPreferences preferences;
+    public SharedPreferences.Editor editor;
+    public String SHARED_PREF_NAME = "pos_pref";
+    public String KEY_TOKEN = "Token";
+    public String MERCHANT_ID = "merchantId";
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -26,6 +49,39 @@ public class CustomersFragment extends Fragment {
             startActivity(i);
         });
 
+        preferences = this.getContext().getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE);
+        token = preferences.getString(KEY_TOKEN, "Token");
+        merchantId = Integer.valueOf(preferences.getString(MERCHANT_ID, "merchantId"));
+
+        String url = Constants.BASE_URL + "Customer/GetCustomerList?MerchantId=";
+        getCustomerList(url);
+
         return binding.getRoot();
+    }
+
+    private void getCustomerList(String url) {
+        StringRequest stringRequest  = new StringRequest(Request.Method.GET,
+                url + merchantId, response -> Log.d("response customer", response), new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json");
+                headers.put("Authorization", "Bearer " + token);
+                return headers;
+            }
+
+            @Override
+            public String getBodyContentType() {
+                return "application/json";
+            }
+        };
+        stringRequest.setRetryPolicy(new DefaultRetryPolicy(60000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        MySingleton.getInstance(this.getContext()).addToRequestQueue(stringRequest);
     }
 }
