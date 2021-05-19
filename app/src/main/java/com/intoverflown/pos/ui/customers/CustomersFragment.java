835ff1ad7a9029820d4bdb00ee04cec8a0ccf804
RocqJones <jonesmbindyo@gmail.com>
2021-05-19 20:20:26 +0300

    optimized customer's imports

--- app/src/main/java/com/intoverflown/pos/ui/customers/CustomersFragment.java
+++ app/src/main/java/com/intoverflown/pos/ui/customers/CustomersFragment.java
@@ -13,11 +13,7 @@ import androidx.fragment.app.Fragment;
 
 import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.intoverflown.pos.databinding.FragmentCustomersBinding;
 import com.intoverflown.pos.patterns.MySingleton;
 import com.intoverflown.pos.ui.customers.addcustomers.AddCustomerActivity;
