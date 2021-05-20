7f5cf15bd06a76f047c2b4651e136b71d3cd7ad1
RocqJones <jonesmbindyo@gmail.com>
2021-05-20 22:53:21 +0300

    Supplier adapter done

---
+++ app/src/main/java/com/intoverflown/pos/ui/inventory/adapters/AdapterSupplier.java
@@ -0,0 +1,67 @@
+package com.intoverflown.pos.ui.inventory.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.intoverflown.pos.databinding.ItemSupplierBinding;
+import com.intoverflown.pos.ui.inventory.data.InventoryRemoteData;
+
+import java.util.List;
+
+public class AdapterSupplier extends RecyclerView.Adapter<AdapterSupplier.RvHolder> {
+
+    List<InventoryRemoteData> supplierData;
+    ItemSupplierBinding binding;
+    Context mContext;
+
+    public AdapterSupplier(List<InventoryRemoteData> supplierData, ItemSupplierBinding binding, Context mContext) {
+        this.supplierData = supplierData;
+        this.binding = binding;
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public RvHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        binding = ItemSupplierBinding.inflate(LayoutInflater.from(parent.getContext()));
+        View view = binding.getRoot();
+        return new RvHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RvHolder holder, int position) {
+        holder.sName.setText(supplierData.get(position).getName());
+        holder.address.setText(supplierData.get(position).getAddress());
+        holder.phone.setText(supplierData.get(position).getPhone());
+        holder.email.setText(supplierData.get(position).getEmail());
+        holder.mName.setText(supplierData.get(position).getMerchantName());
+        holder.remarks.setText(supplierData.get(position).getRemarks());
+    }
+
+    @Override
+    public int getItemCount() {
+        return supplierData.size();
+    }
+
+    static class RvHolder extends RecyclerView.ViewHolder {
+
+        TextView sName, address, phone, email, mName, remarks;
+
+        public RvHolder(@NonNull View itemView) {
+            super(itemView);
+            ItemSupplierBinding itemSupplierBinding = ItemSupplierBinding.bind(itemView);
+            sName = itemSupplierBinding.sName;
+            address = itemSupplierBinding.address;
+            phone = itemSupplierBinding.phone;
+            email = itemSupplierBinding.email;
+            mName = itemSupplierBinding.merchantName;
+            remarks = itemSupplierBinding.sRemarks;
+        }
+    }
+}
