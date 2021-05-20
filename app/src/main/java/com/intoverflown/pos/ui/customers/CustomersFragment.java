cf761acd695c276ca955c1592285ec06b0814df0
RocqJones <jonesmbindyo@gmail.com>
2021-05-20 16:22:12 +0300

    CustomersFragment: done creating new customers and displaying list of customers

--- app/src/main/java/com/intoverflown/pos/ui/customers/CustomersFragment.java
+++ app/src/main/java/com/intoverflown/pos/ui/customers/CustomersFragment.java
@@ -10,19 +10,24 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.intoverflown.pos.databinding.FragmentCustomersBinding;
 import com.intoverflown.pos.patterns.MySingleton;
+import com.intoverflown.pos.ui.customers.adapter.AdapterCustomer;
 import com.intoverflown.pos.ui.customers.addcustomers.AddCustomerActivity;
+import com.intoverflown.pos.ui.customers.data.Customer;
 import com.intoverflown.pos.utils.Constants;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static android.content.Context.MODE_PRIVATE;
@@ -40,6 +45,10 @@ public class CustomersFragment extends Fragment {
     public String KEY_TOKEN = "Token";
     public String MERCHANT_ID = "merchantId";
 
+    // adapter data
+    List<Customer> customerData;
+    private AdapterCustomer adapterCustomer;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentCustomersBinding.inflate(inflater, container, false);
@@ -49,6 +58,11 @@ public class CustomersFragment extends Fragment {
             i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             startActivity(i);
         });
+
+        // prepare adapter
+        customerData = new ArrayList<>();
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this.getContext());
+        binding.rvCustomer.setLayoutManager(linearLayoutManager);
 
         preferences = this.getContext().getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE);
         token = preferences.getString(KEY_TOKEN, "Token");
@@ -65,12 +79,28 @@ public class CustomersFragment extends Fragment {
                 url + merchantId, null, response -> {
             try {
                 Log.d("response customer", response.toString());
+
+                JSONObject jsonObjectM = response.getJSONObject("merchant");
+                Log.d("response mName", jsonObjectM.optString("merchantName"));
+                String mName = jsonObjectM.optString("merchantName");
+
                 JSONArray jsonArray = response.getJSONArray("customers");
 
                 for(int i=0; i < jsonArray.length(); i++) {
                     JSONObject jsonObject = (JSONObject) jsonArray.get(i);
                     Log.d("response fname", jsonObject.optString("firstName"));
+                    Customer customer = new Customer();
+                    customer.setFirstName(jsonObject.optString("firstName"));
+                    customer.setOtherNames(jsonObject.optString("otherNames"));
+                    customer.setAddress(jsonObject.optString("address"));
+                    customer.setPhone(jsonObject.optString("phone"));
+                    customer.setEmail(jsonObject.optString("email"));
+                    customer.setMerchantName(mName);
+                    customerData.add(customer);
                 }
+
+                adapterCustomer = new AdapterCustomer(customerData, this.getContext());
+                binding.rvCustomer.setAdapter(adapterCustomer);
             } catch (Exception e){
                 e.printStackTrace();
             }
