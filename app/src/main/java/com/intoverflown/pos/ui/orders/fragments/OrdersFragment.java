4b5fb0a62c6d1ad4a160173b84bb346a8e4b8674
RocqJones <jonesmbindyo@gmail.com>
2021-05-25 15:33:11 +0300

    order fragment nav to add activity

--- app/src/main/java/com/intoverflown/pos/ui/orders/fragments/OrdersFragment.java
+++ app/src/main/java/com/intoverflown/pos/ui/orders/fragments/OrdersFragment.java
@@ -1,5 +1,6 @@
 package com.intoverflown.pos.ui.orders.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +10,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.intoverflown.pos.databinding.FragmentOrdersBinding;
+import com.intoverflown.pos.ui.orders.postorder.AddOrderActivity;
 
 public class OrdersFragment extends Fragment {
 
@@ -17,6 +19,13 @@ public class OrdersFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentOrdersBinding.inflate(inflater, container, false);
+
+        binding.fab.setOnClickListener(v -> {
+            Intent i = new Intent(OrdersFragment.this.getContext(), AddOrderActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(i);
+        });
+
         return binding.getRoot();
     }
 }
