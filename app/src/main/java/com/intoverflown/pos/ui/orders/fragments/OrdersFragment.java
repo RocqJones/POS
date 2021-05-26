ccda9054302400cbc6540c78be0926af98fc1d47
RocqJones <jonesmbindyo@gmail.com>
2021-05-26 13:14:23 +0300

    send order's GET obj request

--- app/src/main/java/com/intoverflown/pos/ui/orders/fragments/OrdersFragment.java
+++ app/src/main/java/com/intoverflown/pos/ui/orders/fragments/OrdersFragment.java
@@ -11,17 +11,24 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.intoverflown.pos.databinding.FragmentOrdersBinding;
 import com.intoverflown.pos.patterns.MySingleton;
+import com.intoverflown.pos.ui.orders.adapter.AdapterOrder;
+import com.intoverflown.pos.ui.orders.data.OrderRemoteData;
 import com.intoverflown.pos.ui.orders.postorder.AddOrderActivity;
 import com.intoverflown.pos.utils.Constants;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static android.content.Context.MODE_PRIVATE;
@@ -42,6 +49,11 @@ public class OrdersFragment extends Fragment {
     int merchantId;
     int customerId;
 
+    List<OrderRemoteData> orderData;
+    private AdapterOrder adapterOrder;
+
+    String unitCost; String discount; String firstName; String otherName; String phone; String address;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentOrdersBinding.inflate(inflater, container, false);
@@ -52,12 +64,16 @@ public class OrdersFragment extends Fragment {
             startActivity(i);
         });
 
+        orderData = new ArrayList<>();
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this.getContext());
+        binding.ordersRecyclerView.setLayoutManager(linearLayoutManager);
+
         preferences = this.getContext().getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE);
         token = preferences.getString(KEY_TOKEN, "Token");
         merchantId = Integer.parseInt(preferences.getString(MERCHANT_ID, "merchantId"));
         customerId = Integer.parseInt(preferences.getString(CUSTOMER_ID, "customerId"));
 
-        String url = Constants.BASE_URL + "Order/?Id=0&CustomerId=  2";
+        String url = Constants.BASE_URL + "Order/?Id=0&CustomerId=";
         getAllOrders(url);
 
         return binding.getRoot();
@@ -66,7 +82,59 @@ public class OrdersFragment extends Fragment {
     private void getAllOrders(String url) {
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
                 url + customerId + "&MerchantId=" + merchantId, null, response -> {
-            Log.d("response", response.toString());
+            try {
+                Log.d("response", response.toString());
+
+                JSONArray jsonArray1 = response.getJSONArray("orderdetail");
+                for (int i = 0; i < jsonArray1.length(); i++) {
+                    JSONObject jsonObject = (JSONObject) jsonArray1.get(i);
+                    unitCost = jsonObject.optString("unitCost");
+                    discount = jsonObject.optString("discountAmount");
+                }
+
+                JSONArray jsonArrayCustomers = response.getJSONArray("customers");
+                for (int j = 0; j < jsonArrayCustomers.length(); j++) {
+                    JSONObject jsonObject = (JSONObject) jsonArrayCustomers.get(j);
+                    firstName = jsonObject.optString("firstName");
+                    otherName = jsonObject.optString("otherNames");
+                    phone = jsonObject.optString("phone");
+                    address = jsonObject.optString("address");
+                }
+
+                JSONArray jsonArrayOrders = response.getJSONArray("orders");
+                for (int k = 0; k < jsonArrayOrders.length(); k++) {
+                    JSONObject jsonObject = (JSONObject) jsonArrayOrders.get(k);
+                    OrderRemoteData orderRemoteData = new OrderRemoteData();
+                    orderRemoteData.setOrderNo(jsonObject.optString("orderNo"));
+
+                    String rawOrdDate = jsonObject.optString("orderDate");
+                    String [] d = rawOrdDate.split("T");
+                    orderRemoteData.setOrderDate(d[0]);
+
+                    String rawRq = jsonObject.optString("dateRequired");
+                    String [] e = rawRq.split("T");
+                    orderRemoteData.setDateRequired(e[0]);
+
+                    String rawShipping = jsonObject.optString("shippingDate");
+                    String [] f = rawShipping.split("T");
+                    orderRemoteData.setShippingDate(f[0]);
+
+                    orderRemoteData.setFirstName(firstName);
+                    orderRemoteData.setOtherNames(otherName);
+                    orderRemoteData.setAddress(address);
+                    orderRemoteData.setPhone(phone);
+                    orderRemoteData.setUnitCost(unitCost);
+                    orderRemoteData.setDiscountAmount(discount);
+                    orderRemoteData.setTotalOrderAmt(jsonObject.optString("totalOrderAmt"));
+
+                    orderData.add(orderRemoteData);
+                }
+
+                adapterOrder = new AdapterOrder(orderData, this.getContext());
+                binding.ordersRecyclerView.setAdapter(adapterOrder);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
         }, error -> {
             error.printStackTrace();
