afe0e21bde3f2ba9a855d5f2c98347044853d1a8
RocqJones <jonesmbindyo@gmail.com>
2021-05-29 18:09:27 +0300

    post data with 400 error

--- app/src/main/java/com/intoverflown/pos/ui/returngoods/fragments/InwardsFragment.java
+++ app/src/main/java/com/intoverflown/pos/ui/returngoods/fragments/InwardsFragment.java
@@ -1,5 +1,6 @@
 package com.intoverflown.pos.ui.returngoods.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +10,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.intoverflown.pos.databinding.FragmentInwardsBinding;
+import com.intoverflown.pos.ui.returngoods.postreturns.AddInwardsActivity;
 
 public class InwardsFragment extends Fragment {
 
@@ -18,6 +20,12 @@ public class InwardsFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentInwardsBinding.inflate(inflater, container, false);
+
+        binding.fab.setOnClickListener(v -> {
+            Intent i = new Intent(this.getContext(), AddInwardsActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(i);
+        });
 
         return binding.getRoot();
     }
