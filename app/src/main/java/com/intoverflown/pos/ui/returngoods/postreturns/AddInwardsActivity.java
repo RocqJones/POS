d7497c71bd592f4ed8535bcde0f3148e2617facc
RocqJones <jonesmbindyo@gmail.com>
2021-06-03 16:28:27 +0300

    optimize imports

--- app/src/main/java/com/intoverflown/pos/ui/returngoods/postreturns/AddInwardsActivity.java
+++ app/src/main/java/com/intoverflown/pos/ui/returngoods/postreturns/AddInwardsActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.DatePicker;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
