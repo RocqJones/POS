419f2a19331e7920bfc236a4507ed994469166d1
RocqJones <jonesmbindyo@gmail.com>
2021-06-14 19:41:25 +0300

    remote data return inwards

---
+++ app/src/main/java/com/intoverflown/pos/ui/returngoods/data/ReturnRemoteData.java
@@ -0,0 +1,94 @@
+package com.intoverflown.pos.ui.returngoods.data;
+
+public class ReturnRemoteData {
+    private int id;
+    private String orderDate;
+    private String dateDelivered;
+    private String orderedQuantity;
+    private String returnedQuantity;
+    private String returnDate;
+    private String returnReason;
+    private String customer;
+    private String supplier;
+    private String product;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(String orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public String getDateDelivered() {
+        return dateDelivered;
+    }
+
+    public void setDateDelivered(String dateDelivered) {
+        this.dateDelivered = dateDelivered;
+    }
+
+    public String getOrderedQuantity() {
+        return orderedQuantity;
+    }
+
+    public void setOrderedQuantity(String orderedQuantity) {
+        this.orderedQuantity = orderedQuantity;
+    }
+
+    public String getReturnedQuantity() {
+        return returnedQuantity;
+    }
+
+    public void setReturnedQuantity(String returnedQuantity) {
+        this.returnedQuantity = returnedQuantity;
+    }
+
+    public String getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(String returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public String getReturnReason() {
+        return returnReason;
+    }
+
+    public void setReturnReason(String returnReason) {
+        this.returnReason = returnReason;
+    }
+
+    public String getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(String customer) {
+        this.customer = customer;
+    }
+
+    public String getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(String supplier) {
+        this.supplier = supplier;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+}
