a5f2f107ce23ae6ff5cab0e34d321bcd4b54629c
RocqJones <jonesmbindyo@gmail.com>
2021-07-08 17:07:56 +0300

    add merchant dropdown

--- app/src/main/java/com/intoverflown/pos/ui/profile/addmerchant/AddMerchantActivity.java
+++ app/src/main/java/com/intoverflown/pos/ui/profile/addmerchant/AddMerchantActivity.java
@@ -5,23 +5,29 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
 import com.intoverflown.pos.MainActivity;
 import com.intoverflown.pos.databinding.ActivityAddMerchantBinding;
+import com.intoverflown.pos.patterns.MySingleton;
 import com.intoverflown.pos.utils.Constants;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class AddMerchantActivity extends AppCompatActivity {
@@ -34,7 +40,8 @@ public class AddMerchantActivity extends AppCompatActivity {
     public String SHARED_PREF_NAME = "pos_pref";
     public String KEY_ID = "Id";
     public String KEY_TOKEN = "Token";
-    public String MerchantTypeId = "TypeA";
+
+    List<String> mutableArrCountry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,16 +49,26 @@ public class AddMerchantActivity extends AppCompatActivity {
         binding = ActivityAddMerchantBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        mutableArrCountry = new ArrayList<String>();
+
+        preferences = this.getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE);
+
+        uid = preferences.getString(KEY_ID, "Id");
+        token = preferences.getString(KEY_TOKEN, "Token");
+
+        // countries
+        getCountryList();
+
         binding.merchantBackBtn.setOnClickListener(v -> {
             Intent i = new Intent(AddMerchantActivity.this, MainActivity.class);
             i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             startActivity(i);
         });
 
-        preferences = this.getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE);
-
-        uid = preferences.getString(KEY_ID, "Id");
-        token = preferences.getString(KEY_TOKEN, "Token");
+        String[] merchantTypes = {"Select...", "TypeA", "TypeB"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, merchantTypes);
+        binding.merchantType.setAdapter(adapter);
 
         Log.d("uid category", uid);
         Log.d("token category", token);
@@ -62,13 +79,59 @@ public class AddMerchantActivity extends AppCompatActivity {
         });
     }
 
+    private void getCountryList() {
+        String countryUrl = Constants.BASE_URL + "Code/GetCountry";
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET,
+                countryUrl, null, response -> {
+            try {
+                Log.d("response", response.toString());
+
+                for (int i = 0; i < response.length(); i++) {
+                    JSONObject jsonObject = (JSONObject) response.get(i);
+                    String txt = jsonObject.optString("text");
+                    mutableArrCountry.add(txt);
+                }
+                Log.d("arr", mutableArrCountry.toString());
+
+                String[] countries = mutableArrCountry.toArray(new String[mutableArrCountry.size()]);
+                Log.d("arrStr", String.valueOf(countries));
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                        android.R.layout.simple_dropdown_item_1line, countries);
+                binding.merchantCountries.setAdapter(adapter);
+
+            } catch (Exception e) {
+                Log.i("profile", Log.getStackTraceString(e));
+            }
+        } , error -> {
+            Log.e("error", error.toString());
+            Toast.makeText(AddMerchantActivity.this, "loading failed!", Toast.LENGTH_SHORT).show();
+        }) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("Content-Type", "application/json");
+                params.put("Authorization", "Bearer " + token);
+                return params;
+            }
+
+            @Override
+            public String getBodyContentType() {
+                return "application/json";
+            }
+        };
+        jsonArrayRequest.setRetryPolicy(new DefaultRetryPolicy(60000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        MySingleton.getInstance(this).addToRequestQueue(jsonArrayRequest);
+    }
+
     private void createMerchant(String url) {
         RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
         JSONArray array = new JSONArray();
         JSONObject jsonObjects = new JSONObject();
 
         String name = binding.merchantName.getText().toString().trim();
-        String country = binding.merchantCountry.getText().toString().trim();
+        String country = binding.merchantCountries.getSelectedItem().toString().trim();
+        String merchantType = binding.merchantType.getSelectedItem().toString().trim();
 
         if (name.isEmpty() && country.isEmpty()) {
             Toast.makeText(this, "Enter all fields!!", Toast.LENGTH_SHORT).show();
@@ -79,7 +142,7 @@ public class AddMerchantActivity extends AppCompatActivity {
 
             try {
                 jsonObjects.put("MerchantName", name);
-                jsonObjects.put("MerchantTypeId", MerchantTypeId);
+                jsonObjects.put("MerchantTypeId", merchantType);
                 jsonObjects.put("CountryId", country);
                 jsonObjects.put("createdById", uid);
 
