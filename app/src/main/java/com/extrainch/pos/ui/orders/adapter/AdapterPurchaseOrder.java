b4aa9a8033b9648834ac7bdf1c2fcf77236a0cda
RocqJones <jonesmbindyo@gmail.com>
2021-07-20 22:24:10 +0300

    adapter purchase order

---
+++ app/src/main/java/com/extrainch/pos/ui/orders/adapter/AdapterPurchaseOrder.java
@@ -0,0 +1,34 @@
+package com.extrainch.pos.ui.orders.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.jetbrains.annotations.NotNull;
+
+public class AdapterPurchaseOrder extends RecyclerView.Adapter<AdapterPurchaseOrder.RvHolder> {
+
+    @NotNull
+    @Override
+    public AdapterPurchaseOrder.RvHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AdapterPurchaseOrder.RvHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class RvHolder extends RecyclerView.ViewHolder {
+        public RvHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
