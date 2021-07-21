12f9ed347ecedabf205db9d5892128e9a6e16e5a
Jones Mbindyo <jonesmbindyo@gmail.com>
2021-07-21 19:10:44 +0300

    order management

--- app/src/main/java/com/extrainch/pos/ui/orders/fragments/PurchaseOrderFragment.java
+++ app/src/main/java/com/extrainch/pos/ui/orders/fragments/PurchaseOrderFragment.java
@@ -11,7 +11,12 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.extrainch.pos.databinding.FragmentPurchaseOrderBinding;
+import com.extrainch.pos.ui.orders.data.OrderRemoteData;
+import com.extrainch.pos.ui.orders.data.PurchaseRemoteData;
 import com.extrainch.pos.ui.orders.postorder.AddPurchaseOrderActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PurchaseOrderFragment extends Fragment {
 
@@ -22,14 +27,27 @@ public class PurchaseOrderFragment extends Fragment {
     public String SHARED_PREF_NAME = "pos_pref";
     public String KEY_TOKEN = "Token";
 
+    List<PurchaseRemoteData> purchaseRemoteData;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentPurchaseOrderBinding.inflate(inflater, container, false);
+
+        purchaseRemoteData = new ArrayList<>();
 
         binding.fab.setOnClickListener(v -> {
             Intent i = new Intent(this.getContext(), AddPurchaseOrderActivity.class);
             startActivity(i);
         });
+
+        // temp
+        if (purchaseRemoteData.isEmpty()) {
+            binding.quoteRecyclerView.setVisibility(View.GONE);
+            binding.noData.setVisibility(View.VISIBLE);
+        } else {
+            binding.quoteRecyclerView.setVisibility(View.VISIBLE);
+            binding.noData.setVisibility(View.GONE);
+        }
 
         return binding.getRoot();
     }
