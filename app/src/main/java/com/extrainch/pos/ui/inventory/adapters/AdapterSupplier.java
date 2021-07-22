1ca59cc520a9e7929fbc61787d52ced96ac12f6a
RocqJones <jonesmbindyo@gmail.com>
2021-07-22 22:48:15 +0300

    supplier adapter edit done

--- app/src/main/java/com/extrainch/pos/ui/inventory/adapters/AdapterSupplier.java
+++ app/src/main/java/com/extrainch/pos/ui/inventory/adapters/AdapterSupplier.java
@@ -1,24 +1,39 @@
 package com.extrainch.pos.ui.inventory.adapters;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.extrainch.pos.R;
 import com.extrainch.pos.databinding.ItemSupplierBinding;
 import com.extrainch.pos.ui.inventory.data.InventoryRemoteData;
+import com.extrainch.pos.ui.inventory.fragments.SupplierFragment;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
 
+import static android.content.Context.MODE_PRIVATE;
+
 public class AdapterSupplier extends RecyclerView.Adapter<AdapterSupplier.RvHolder> {
+
+    public SharedPreferences preferences;
+    public String SHARED_PREF_NAME = "pos_pref";
+    public String KEY_TOKEN = "Token";
 
     List<InventoryRemoteData> supplierData;
     ItemSupplierBinding binding;
     Context mContext;
+
+    SupplierFragment supplierFragment = new SupplierFragment();
 
     public AdapterSupplier(List<InventoryRemoteData> supplierData, Context mContext) {
         this.supplierData = supplierData;
@@ -41,6 +56,53 @@ public class AdapterSupplier extends RecyclerView.Adapter<AdapterSupplier.RvHold
         holder.email.setText(supplierData.get(position).getEmail());
         holder.mName.setText(supplierData.get(position).getMerchantName());
         holder.remarks.setText(supplierData.get(position).getRemarks());
+        holder.editBtn.setOnClickListener(v -> {
+            String sId = supplierData.get(position).getItemCatId();
+            String mId = supplierData.get(position).getMerchantId();
+            String txt1 = supplierData.get(position).getName();
+            String txt2 = supplierData.get(position).getAddress();
+            String txt3 = supplierData.get(position).getPhone();
+            String txt4 = supplierData.get(position).getEmail();
+            String txt5 = supplierData.get(position).getRemarks();
+            String createdBy = supplierData.get(position).getCreatedById();
+
+            preferences = mContext.getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE);
+            String tokenS = preferences.getString(KEY_TOKEN, "Token");
+
+            // start dialog
+            final Dialog dialog = new Dialog(mContext);
+            dialog.setContentView(R.layout.edit_supplier);
+            dialog.setCancelable(false);
+
+            EditText name = (EditText) dialog.findViewById(R.id.name);
+            EditText address = (EditText) dialog.findViewById(R.id.address);
+            EditText phone = (EditText) dialog.findViewById(R.id.phone);
+            EditText email = (EditText) dialog.findViewById(R.id.email);
+            EditText remarks = (EditText) dialog.findViewById(R.id.remarks);
+
+            Button svBtn = (Button) dialog.findViewById(R.id.saveBtn);
+
+            name.setText(txt1);
+            address.setText(txt2);
+            phone.setText(txt3);
+            remarks.setText(txt4);
+            email.setText(txt5);
+
+            svBtn.setOnClickListener(v1 -> {
+                dialog.dismiss();
+                String gNm = name.getText().toString().trim();
+                String gAd = address.getText().toString().trim();
+                String gPh = phone.getText().toString().trim();
+                String gEm = email.getText().toString().trim();
+                String gRm = remarks.getText().toString().trim();
+
+                supplierFragment.modifyData(sId, mId, gNm, gAd, gPh, gEm, gRm, createdBy, tokenS);
+            });
+
+            dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation_1;
+            dialog.show();
+            dialog.setCanceledOnTouchOutside(true);
+        });
     }
 
     @Override
@@ -51,6 +113,7 @@ public class AdapterSupplier extends RecyclerView.Adapter<AdapterSupplier.RvHold
     static class RvHolder extends RecyclerView.ViewHolder {
 
         TextView sName, address, phone, email, mName, remarks;
+        FloatingActionButton editBtn;
 
         public RvHolder(@NonNull View itemView) {
             super(itemView);
@@ -61,6 +124,7 @@ public class AdapterSupplier extends RecyclerView.Adapter<AdapterSupplier.RvHold
             email = itemSupplierBinding.email;
             mName = itemSupplierBinding.merchantName;
             remarks = itemSupplierBinding.sRemarks;
+            editBtn = itemSupplierBinding.edit;
         }
     }
 }
