82ae5e575086b30d0c83077282a32bbece1fc76e
Jones Mbindyo <jonesmbindyo@gmail.com>
2021-08-03 19:16:25 +0300

    home fragment

--- app/src/main/java/com/extrainch/pos/ui/home/HomeFragment.java
+++ app/src/main/java/com/extrainch/pos/ui/home/HomeFragment.java
@@ -10,10 +10,14 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.extrainch.pos.databinding.FragmentHomeBinding;
+import com.extrainch.pos.ui.category.CategoryActivity;
 import com.extrainch.pos.ui.inventory.InventoryActivityMain;
 import com.extrainch.pos.ui.merchantbranch.MerchantBranchActivity;
 import com.extrainch.pos.ui.orders.OrdersActivity;
+import com.extrainch.pos.ui.products.ProductActivity;
+import com.extrainch.pos.ui.purchase.PurchaseActivity;
 import com.extrainch.pos.ui.returngoods.InwardsOutwardsActivity;
+import com.extrainch.pos.ui.supplier.SupplierActivity;
 
 public class HomeFragment  extends Fragment {
 
@@ -35,12 +39,6 @@ public class HomeFragment  extends Fragment {
             startActivity(i);
         });
 
-//        binding.homeSalesExp.setOnClickListener(v -> {
-//            Intent j = new Intent(HomeFragment.this.getContext(), SalesAndExpenses.class);
-//            j.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//            startActivity(j);
-//        });
-
         binding.homeMerchants.setOnClickListener(v -> {
             Intent k = new Intent(HomeFragment.this.getContext(), MerchantBranchActivity.class);
             k.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
@@ -57,6 +55,30 @@ public class HomeFragment  extends Fragment {
             Intent m = new Intent(HomeFragment.this.getContext(), InwardsOutwardsActivity.class);
             m.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             startActivity(m);
+        });
+
+        binding.homeProduct.setOnClickListener(v -> {
+            Intent p = new Intent(HomeFragment.this.getContext(), ProductActivity.class);
+            p.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(p);
+        });
+
+        binding.homeCategory.setOnClickListener(v -> {
+            Intent cT = new Intent(HomeFragment.this.getContext(), CategoryActivity.class);
+            cT.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(cT);
+        });
+
+        binding.homeSupplier.setOnClickListener(v -> {
+            Intent sP = new Intent(HomeFragment.this.getContext(), SupplierActivity.class);
+            sP.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(sP);
+        });
+
+        binding.homePurchase.setOnClickListener(v -> {
+            Intent pC = new Intent(HomeFragment.this.getContext(), PurchaseActivity.class);
+            pC.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(pC);
         });
     }
 }
