de98cde19b76efeea758b5a1f919cf3afb0225af
Jones Mbindyo <jonesmbindyo@gmail.com>
2021-08-04 18:15:34 +0300

    printer fragment

--- app/src/main/java/com/extrainch/pos/ui/printer/PrinterFragment.java
+++ app/src/main/java/com/extrainch/pos/ui/printer/PrinterFragment.java
@@ -1,16 +1,20 @@
 package com.extrainch.pos.ui.printer;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.extrainch.pos.R;
 import com.extrainch.pos.databinding.FragmentPrinterBinding;
 import com.extrainch.pos.repository.PurchaseRemoteData;
 import com.extrainch.pos.ui.printer.addpurchasereceipt.AddPurchaseReceiptActivity;
@@ -54,5 +58,22 @@ public class PrinterFragment extends Fragment {
         }
 
         return binding.getRoot();
+    }
+
+    private void warnDialog(String message) {
+        final Dialog dialog = new Dialog(this.getContext());
+        dialog.setContentView(R.layout.dialog_warning);
+        dialog.setCancelable(false);
+
+        TextView warnMessage = (TextView) dialog.findViewById(R.id.warnMessage);
+        Button okBtn = (Button) dialog.findViewById(R.id.okBtn);
+
+        warnMessage.setText(message);
+
+        okBtn.setOnClickListener(v -> dialog.dismiss());
+
+        dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation_1;
+        dialog.show();
+        dialog.setCanceledOnTouchOutside(true);
     }
 }
